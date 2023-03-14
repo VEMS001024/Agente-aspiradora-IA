c91024a33ec19ef2173a073f4a973854c612221e
VEMS001024 <116967805+VEMS001024@users.noreply.github.com>
2023-03-14 12:37:55 -0600

    Add files via upload
    version de miercoles del 15 de mar

--- agente.py
+++ agente.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+import time
 
 # configurar la ventana
 window = tk.Tk()
@@ -12,29 +13,71 @@ canvas = tk.Canvas(window, width=900, height=900)
 
 # dibujar la cuadrícula
 for i in range(0, 901, 30):
-    canvas.create_line(i, 0, i, 900, fill="#ddd")
-    canvas.create_line(0, i, 900, i, fill="#ddd")
+    canvas.create_line(i, 0, i, 900, fill="#ccc")
+    canvas.create_line(0, i, 900, i, fill="#ccc")
 
-# agregar un punto
-x, y = 75, 45 #Centro del punto
-r = 14
-canvas.create_oval(x-r, y-r, x+r, y+r, fill="red")
+# agregar unos puntos
+#for i in range(random.randrange(5, 15)):
+#    x = (random.randrange(0, 30)) * 30 + 15 #Centro del punto
+#    y = (random.randrange(0, 30)) * 30 + 15 #Centro del punto
+#    r = 14
+    
+#    canvas.create_oval(x-r, y-r, x+r, y+r, fill="red")
+
+# cargar la imagen basura
+basura = []
+
+imagebasura = tk.PhotoImage(file="imagenes/basura.png")
+imagebasura = imagebasura.subsample(imagebasura.height() // 30 )  # reducir a 30 x **
+
+for i in range(random.randrange(5, 15)):
+    x = (random.randrange(0, 30)) * 30 #Esquina izquierda
+    y = (random.randrange(0, 30)) * 30 #Esquina izquierda
+
+    basura.append((x, y))
+
+    img_obj_basura = canvas.create_image(x, y, image=imagebasura, anchor="nw")
 
 # cargar la imagen
 image = tk.PhotoImage(file="imagenes/aspiradora.png")
-image = image.subsample(image.width() // 30)  # reducir a 60 x **
+image = image.subsample(image.width() // 30)  # reducir a 30 x **
 x, y = 120, 30 #Esquina izquierda
 
 img_obj = canvas.create_image(x, y, image=image, anchor="nw")
+inicio = False
 
 # mover la imagen con clics
 def move_image(event):
-    global x, y
-    x = random.randrange(0, 900/30) * 10
-    y = random.randrange(0, 900/30) * 10
-    canvas.coords(img_obj, x, y)
+    global inicio
+
+    inicio = not inicio
+    print("CLIC", inicio)
 
 canvas.bind("<Button-1>", move_image)
+print("CLIC", inicio)
+while inicio:
+
+    x += (random.randrange(-1, 1)) * 30
+    y += (random.randrange(-1, 1)) * 30
+
+    if x < 0:
+        x = 870
+
+    if x > 870:
+        x = 0
+
+    if y < 0:
+        y = 870
+
+    if y > 870:
+        y = 0
+    
+    print(x, y)
+
+    canvas.coords(img_obj, x, y)
+    time.sleep(0.5)
+
+
 
 # mostrar el lienzo
 canvas.pack()
